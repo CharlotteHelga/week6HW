6e9e6ece441b244d3d3181fe96381609ed54fdec
CharlotteHelga <114713460+CharlotteHelga@users.noreply.github.com>
2024-01-14 12:19:41 +0300

    Javascript weather changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let humidElement = document.querySelector("#humidity");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#current-temperature-icon");
+
+  console.log(response.data);
+  let temperature = Math.round(response.data.temperature.current);
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  iconElement.innerHTML = response.data.condition.icon;
 }
 
 function search(event) {
@@ -11,9 +22,8 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "14e8b2oefd0f8a35ca5498abfa5edtb2";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayTemperature);
 }
 
